99aebe4bbbb7280adb154eaec00743e2a6127fd8
Jessica Lee <jlee22@mit.edu>
2020-04-26 00:54:10 -0400

    added global styling, Layout elements

--- pages/posts/input.js
+++ pages/posts/input.js
@@ -1,14 +1,49 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+function DayInput(props) {
+  return (
+    <div className="day">
+        <div className = "dayName"> 
+            <p>{props.dayName}</p>
+        </div>
+        <select>
+            <option value="0:30">0:30 </option>
+            <option value="1:00">1:00 </option>
+            <option value="1:30">1:30 </option>
+            <option value="2:00">2:00 </option>
+        </select>
+    </div>
+  );
+}
+
 
 export default function Input() {
-    return (
-        <>
-          <h1>First Post</h1>
-          <h2>
-            <Link href="/">
-              <a>Back to home</a>
-            </Link>
-          </h2>
-        </>
-      )
-  }
+  return (
+    <Layout>
+      <Head>
+        <title> Enter how much time each day? </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>First Post</h1>
+      <div className = "week">
+        <DayInput dayName = "Monday"/>
+        <DayInput dayName = "Tuesday"/>
+        <DayInput dayName = "Wednesday"/>
+        <DayInput dayName = "Thursday"/>
+        <DayInput dayName = "Friday"/>
+      </div>
+    </Layout>
+  );
+}
+
+<style jsx>{`
+        .week {
+            display: flex;
+            background-color: DodgerBlue;
+            align-items: center;
+        }
+        .day {
+            width: 100px;
+        }
+      `}</style>
